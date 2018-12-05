fa00d314a6d6b187ca152ba12ece269e96da828d
ML <bunglenutter@gmail.com>
2018-12-05 21:42:13 +0000

    Setting up Enzyme

--- src/components/__tests__/Header.spec.js
+++ src/components/__tests__/Header.spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../Header';
+import { shallow } from 'enzyme';
 
 describe('Header component', () => {
     it('renders', () => {
         const div = document.createElement('div');
         ReactDOM.render(<Header />, div);
+    });
+
+    it('contains a header element', () => {
+        const wrapper = shallow(<Header/>);
+        expect(wrapper.find('header').length).toEqual(1);
     });
 });
