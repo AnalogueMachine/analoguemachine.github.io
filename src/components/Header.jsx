8849b2509a419142ba96dbd6faf6b3c9804baf46
ML <bunglenutter@gmail.com>
2018-12-10 22:55:30 +0000

    Adding links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class Header extends Component {
     render() {
         return (
-            <header></header>
+            <header>
+                <a href='/' id='logolink' className='logo'>Home</a>
+                <a href='/about' id='aboutmelink' className='button'>ABOUT</a>
+            </header>
         )
     }
 }
--- src/components/__tests__/Header.spec.js
+++ src/components/__tests__/Header.spec.js
@@ -13,4 +13,47 @@ describe('Header component', () => {
         const wrapper = shallow(<Header/>);
         expect(wrapper.find('header').length).toEqual(1);
     });
+
+    describe('First link', () => {
+        let wrapper, homeLink;
+
+        beforeEach(() => {
+            wrapper = shallow(<Header/>);
+            homeLink = wrapper.find('#logolink');
+        });
+
+        it('should contain a logo', () => {
+            expect(homeLink).not.toBe(undefined);
+            expect(homeLink.props().className).toBe('logo');
+        });
+
+        it('should link to the home page', () => {
+            expect(homeLink.props().href).toBe('/');
+        })
+    });
+
+    describe('Second link', () => {
+        let wrapper, aboutMeLink;
+
+        beforeEach(() => {
+            wrapper = shallow(<Header/>);
+            aboutMeLink = wrapper.find('#aboutmelink');
+        });
+
+        it('should exist', () => {
+            expect(aboutMeLink.length).toBe(1);
+        })
+
+        it('should contain the appropriate text', () => {
+            expect(aboutMeLink.props().children).toBe('ABOUT');
+        });
+
+        it('should link to the about page', () => {
+            expect(aboutMeLink.props().href).toBe('/about');
+        });
+
+        it('should be a button', () => {
+            expect(aboutMeLink.props().className).toBe('button');
+        });
+    });
 });
