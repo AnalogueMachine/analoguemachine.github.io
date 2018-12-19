f5d3e1df81e84fc93cdde484eafbb9adbbf38fe1
ML <bunglenutter@gmail.com>
2018-12-19 23:46:25 +0000

    Makes header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,20 @@ import { Link } from 'react-router-dom';
 export default class Header extends Component {
   render() {
     return (
-      <header>
+      <header className="row">
         <Link to='/' id='homelink' className='logo'>analoguemachine</Link>
-        <Link to='/about' id='aboutlink' className='button'>ABOUT</Link>
-        <a href='https://github.com/AnalogueMachine' id='githublink' className='button'>GITHUB</a>
-        <a href='https://twitter.com/AnalogueMachine' id='twitterlink' className='button'>TWITTER</a>
+        <Link to='/about' id='aboutlink' className='button col-sm col-md'>
+          <span className="icon-user"></span>
+          <span className="headerlinktext">ABOUT</span>
+        </Link>
+        <a href='https://github.com/AnalogueMachine' id='githublink' className='button col-sm col-md'>
+          <span className="icon-share"></span>
+          <span className="headerlinktext">GITHUB</span>
+        </a>
+        <a href='https://twitter.com/AnalogueMachine' id='twitterlink' className='button col-sm col-md'>
+          <span className="icon-rss"></span>
+          <span className="headerlinktext">TWITTER</span>
+        </a>
       </header>
     )
   }
--- src/components/__tests__/Header.spec.js
+++ src/components/__tests__/Header.spec.js
@@ -18,6 +18,10 @@ describe('Header component', () => {
         expect(wrapper.find('header').length).toEqual(1);
     });
 
+    it('has a header with a responsive class name', () => {
+        expect(wrapper.find('header').props().className).toBe('row');
+    });
+
     describe('Home link', () => {
         it('should exist', () => {
             expect(homeLink).not.toBe(undefined);
@@ -28,7 +32,7 @@ describe('Header component', () => {
         });
 
         it('should be a react-router link', () => {
-            expect(homeLink.type()).toBe(Link);
+            expect(homeLink.type()).toBe(Link); 
         });
 
         it('should link to the home page', () => {
@@ -49,16 +53,36 @@ describe('Header component', () => {
             expect(aboutLink.type()).toBe(Link);
         });
 
-        it('should contain the appropriate text', () => {
-            expect(aboutLink.props().children).toBe('ABOUT');
-        });
-
         it('should link to the about page', () => {
             expect(aboutLink.props().to).toBe('/about');
         });
 
-        it('should be a button', () => {
-            expect(aboutLink.props().className).toBe('button');
+        it('should be a responsive button', () => {
+            expect(aboutLink.props().className).toBe('button col-sm col-md');
+        });
+
+        describe('icon', () => {
+            it('should contain a span element for an icon', () => {
+                expect(aboutLink.props().children[0].type).toBe('span');
+            });
+    
+            it('should contain an appropriate image in the icon span', () => {
+                expect(aboutLink.props().children[0].props.className).toBe('icon-user');
+            });
+        });
+        
+        describe('link text', () => {
+            it('should contain a span element for the link text', () => {
+                expect(aboutLink.props().children[1].type).toBe('span');
+            });
+    
+            it('should disappear when the screen gets too small', () => {
+                expect(aboutLink.props().children[1].props.className).toBe('headerlinktext');
+            });
+    
+            it('should contain the appropriate text', () => {
+                expect(aboutLink.props().children[1].props.children).toBe('ABOUT');
+            });
         });
     });
 
@@ -67,34 +91,74 @@ describe('Header component', () => {
             expect(githubLink.length).toBe(1);
         });
 
-        it('should contain the appropriate text', () => {
-            expect(githubLink.props().children).toBe('GITHUB');
-        });
-
         it('should link to the about page', () => {
             expect(githubLink.props().href).toBe('https://github.com/AnalogueMachine');
         });
 
-        it('should be a button', () => {
-            expect(githubLink.props().className).toBe('button');
+        it('should be a responsive button', () => {
+            expect(githubLink.props().className).toBe('button col-sm col-md');
+        });
+
+        describe('link icon', () => {
+            it('should contain a span element for an icon', () => {
+                expect(githubLink.props().children[0].type).toBe('span');
+            });
+    
+            it('should contain an appropriate image', () => {
+                expect(githubLink.props().children[0].props.className).toBe('icon-share');
+            });
+        });
+
+        describe('link text', () => {
+            it('should contain a span element for the link text', () => {
+                expect(githubLink.props().children[1].type).toBe('span');
+            });
+    
+            it('should disappear when the screen gets too small', () => {
+                expect(githubLink.props().children[1].props.className).toBe('headerlinktext');
+            });
+    
+            it('should contain the appropriate text', () => {
+                expect(githubLink.props().children[1].props.children).toBe('GITHUB');
+            });
         });
     });
 
     describe('Twitter link', () => {
         it('should exist', () => {
             expect(twitterLink.length).toBe(1);
-        })
-
-        it('should contain the appropriate text', () => {
-            expect(twitterLink.props().children).toBe('TWITTER');
         });
-
+        
         it('should link to the about page', () => {
             expect(twitterLink.props().href).toBe('https://twitter.com/AnalogueMachine');
         });
 
-        it('should be a button', () => {
-            expect(twitterLink.props().className).toBe('button');
+        it('should be a responsive button', () => {
+            expect(twitterLink.props().className).toBe('button col-sm col-md');
+        });
+
+        describe('link icon', () => {
+            it('should contain a span element for an icon', () => {
+                expect(twitterLink.props().children[0].type).toBe('span');
+            });
+    
+            it('should contain an appropriate image', () => {
+                expect(twitterLink.props().children[0].props.className).toBe('icon-rss');
+            })
+        });;
+
+        describe('link text', () => {
+            it('should contain a span element for the link text', () => {
+                expect(twitterLink.props().children[1].type).toBe('span');
+            });
+    
+            it('should disappear when the screen gets too small', () => {
+                expect(twitterLink.props().children[1].props.className).toBe('headerlinktext');
+            });
+    
+            it('should contain the appropriate text', () => {
+                expect(twitterLink.props().children[1].props.children).toBe('TWITTER');
+            });
         });
     });
 });
