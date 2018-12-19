422fcd1de6cb5dba14b6c513213c7d1246b15420
ML <bunglenutter@gmail.com>
2018-12-19 23:46:00 +0000

    Adds text disappearing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import './App.css';
+import './App.css';
 import PrimaryLayout from './components/PrimaryLayout';
 import { BrowserRouter } from 'react-router-dom';
 
