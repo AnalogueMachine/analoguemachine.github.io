8ee7ad0e722b0b3ca76feba219ae2c00937db791
ML <bunglenutter@gmail.com>
2018-12-23 01:03:44 +0000

    Fleshes out Home Page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Logo from '../human.png';
+import TechCard from './TechCard';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div>Home Page</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm">
+            <img src={Logo} alt="Main logo" className="maintitle"></img>
+            <h1 className="maintitle">
+              analoguemachine
+            </h1>
+            <p className="subtitle">A software developer working in Belfast</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/components/__tests__/HomePage.spec.js
+++ src/components/__tests__/HomePage.spec.js
@@ -12,4 +12,65 @@ describe('Home Page', () => {
     it('should exist', () => {
         expect(wrapper).not.toBe(undefined);
     });
+
+    it('should return a container div', () => {
+        const innerDiv = wrapper.getElement();
+
+        expect(innerDiv.type).toBe('div');
+        expect(innerDiv.props.className).toBe('container');
+    });
+
+    describe('Container', () => {
+        let container;
+
+        beforeEach(() => {
+            container = wrapper.getElement();
+        });
+
+        describe('First row', () => {
+            let row;
+
+            beforeEach(() => {
+                row = container.props.children;
+            });
+
+            it('should contain a row with a centred div for the title', () => {
+                expect(row.props.className).toBe('row');
+                expect(row.props.children.props.className).toBe('col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm');
+            });
+
+            describe('Main title', () => {
+                let titleImageElement, titleElement, subtitleElement;
+
+                beforeEach(() => {
+                    titleImageElement = row.props.children.props.children[0]
+                    titleElement = row.props.children.props.children[1];
+                    subtitleElement = row.props.children.props.children[2];
+                });
+
+                it('should contain a title image', () => {
+                    expect(titleImageElement.type).toBe('img');
+                    expect(titleImageElement.props.src).toBe('human.png');
+                    expect(titleImageElement.props.alt).toBe('Main logo');
+                    expect(titleImageElement.props.className).toBe('maintitle');
+                });
+
+                it('should contain a heading element with the main title', () => {
+                    expect(titleElement.type).toBe('h1');
+                    expect(titleElement.props.className).toBe('maintitle');
+                    expect(titleElement.props.children).toBe('analoguemachine');
+                });
+
+                it('should contain a subtitle', () => {
+                    expect(subtitleElement.type).toBe('p');
+                    expect(subtitleElement.props.className).toBe('subtitle');
+                    expect(subtitleElement.props.children).toBe('A software developer working in Belfast');
+                });
+            });
+        });
+
+        describe('Second row', () => {
+            // Tech cards
+        })
+    })
 });
