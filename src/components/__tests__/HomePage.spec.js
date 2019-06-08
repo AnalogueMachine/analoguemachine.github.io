468d7112b1eba169ecb0f5943a2fdfe3fe821f72
-__- <6825625+HundredPercentCoverage@users.noreply.github.com>
2019-06-08 09:42:56 +0100

    Update HomePage.spec.js

--- src/components/__tests__/HomePage.spec.js
+++ src/components/__tests__/HomePage.spec.js
@@ -69,7 +69,7 @@ describe('Home Page', () => {
                 it('should contain a heading element with the main title', () => {
                     expect(titleElement.type).toBe('h1');
                     expect(titleElement.props.className).toBe('maintitle');
-                    expect(titleElement.props.children).toBe('GoodCoverage');
+                    expect(titleElement.props.children).toBe('HundredPercentCoverage');
                 });
 
                 it('should contain a subtitle', () => {
