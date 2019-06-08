02c3305d8502af6aeb48e511e2abc4d6bec416cf
-__- <6825625+HundredPercentCoverage@users.noreply.github.com>
2019-06-08 09:43:31 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,16 @@ export default class Header extends Component {
   render() {
     return (
       <header className="row sticky">
-        <Link to='/' id='homelink' className='logo'>GoodCoverage</Link>
+        <Link to='/' id='homelink' className='logo'>HPC</Link>
         <Link to='/about' id='aboutlink' className='button col-sm col-md'>
           <User size={ICON_SIZE} style={{verticalAlign: "sub"}}/>
           <span className="headerlinktext"> ABOUT</span>
         </Link>
-        <a href='https://github.com/GoodCoverage' id='githublink' className='button col-sm col-md'>
+        <a href='https://github.com/HundredPercentCoverage' id='githublink' className='button col-sm col-md'>
           <GitHub size={ICON_SIZE} style={{verticalAlign: "sub"}}/>
           <span className="headerlinktext"> GITHUB</span>
         </a>
-        <a href='https://twitter.com/GoodCoverage' id='twitterlink' className='button col-sm col-md'>
+        <a href='https://twitter.com/' id='twitterlink' className='button col-sm col-md'>
           <Twitter size={ICON_SIZE} style={{verticalAlign: "sub"}}/>
           <span className="headerlinktext"> TWITTER</span>
         </a>
